5442c808e65e6861f1519a231a0c2b82fee91218
mariacividati <132068380+mariacividati@users.noreply.github.com>
2024-09-19 08:43:29 -0300

    Add files via upload

---
+++ mariaflahcard/assets/app.js
@@ -0,0 +1,9 @@
+function criaCartao (categoria, pergunta, resposta) {
+
+}
+function criaCartao (categoria, pergunta, resposta) {
+    console.log(categoria, pergunta, resposta)
+}
+function criaCartao(categoria, pergunta, resposta) {
+    console.log(categoria, pergunta, resposta)
+}
